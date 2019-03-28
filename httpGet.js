9369dec25de4930cdc62527ac265c24621d0e9de
Alexey Usanin <aleksej.usanin@icloud.com>
2019-03-28 22:14:49 +0200

    Create weather project

--- httpGet.js
+++ httpGet.js
@@ -1,17 +1,14 @@
 const https = require('https');
 function getPost (url) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
   
     https.get(url, (res) => {
-      
       let result = '';
       res.setEncoding('utf-8');
       res.on('data',data =>  result += data);
       res.on('end',() => resolve(JSON.parse(result)));
-      
     });
   });
 }
-
 
 module.exports = getPost;
--- main.js
+++ main.js
@@ -1,18 +1,30 @@
-const date =  new Date().toLocaleString();
+// 1.
+const date = new Date().toLocaleString();
 console.log(date);
 console.log(require('path').basename(__filename));
 
-
+// 2.
 const getNumber = require('./number');
 console.log(getNumber(1, 5));
 
-
+// 3.
 const httpGet = require('./httpGet.js');
 
 httpGet('https://jsonplaceholder.typicode.com/posts')
   .then(response => console.log(response.length))
   .catch(err => console.log(err));
-  
+
+// 4.
 const fs = require('file-system');
 const curDate = new Date();
 fs.writeFile('dir/date.html', curDate);
+
+// 5.
+const http = require('http');
+
+http.get('http://wttr.in/~kharkov', (html) => {
+  html.setEncoding('utf-8');
+  html.on('data', data => fs.appendFile('weather.html', data, (err) => {
+    if (err) throw err;
+  }));
+});
