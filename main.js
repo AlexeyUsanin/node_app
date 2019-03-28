03baf799a46edaa8b50399fd75a009f3436fa818
Alexey Usanin <aleksej.usanin@icloud.com>
2019-03-28 21:08:50 +0200

    CreateDir script

--- main.js
+++ main.js
@@ -13,3 +13,6 @@ httpGet('https://jsonplaceholder.typicode.com/posts')
   .then(response => console.log(response.length))
   .catch(err => console.log(err));
   
+const fs = require('file-system');
+const curDate = new Date();
+fs.writeFile('dir/date.html', curDate);
