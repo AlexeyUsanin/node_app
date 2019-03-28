838e866203bd28dd1acea4b52983a4cc5dfd9966
Alexey Usanin <aleksej.usanin@icloud.com>
2019-03-28 08:23:51 +0200

    Random number

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 const date =  new Date().toLocaleString();
 console.log(date);
 console.log(require('path').basename(__filename));
+
+const getNumber = require('./number');
+console.log(getNumber(1, 5));
