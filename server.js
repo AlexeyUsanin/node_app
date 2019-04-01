8877296967e3f2b345a1d6c4fb8775ae054cb7e9
Alexey Usanin <aleksej.usanin@icloud.com>
2019-04-01 21:10:08 +0300

    Created node server

---
+++ server.js
@@ -0,0 +1,50 @@
+const http = require('http'),
+      fs = require('fs'),
+      ext = ['png', 'jpg', 'ico'],
+      reg = new RegExp(`\\.(${ext.join('|')})$`);
+
+const server = http.createServer((req, res) => {
+  
+  if (reg.test(req.url)) {
+    fs.readFile(req.url.replace('/', ''), (err, data) => {
+      if (err) {
+        res.end('<strong>Error!</strong>');
+      }
+      
+      res.end(data);
+    });
+
+    return;
+  }
+
+  if (req.url.includes('.js')) {
+    res.setHeader('Content-Type', 'text/javascript');
+
+    fs.readFile('img.js', 'utf-8', (err, data) => {
+      if (err) {
+        res.end('<strong>Error!</strong>');
+      }
+      
+      res.end(data);
+    });
+
+    return;
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+
+  fs.readFile('index.html', 'utf-8', (err, data) => {
+    if (err) {
+      res.end('<strong>404! Page not found!</strong>');
+      throw new Error('Error while reading index.html');
+    }
+    
+    res.end(`${data} <h2>${new Date().toLocaleString()}</h2>`);
+  });
+
+});
+
+server.listen(1111, () => {
+  console.log('Server go');
+});
+
